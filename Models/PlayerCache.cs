6ea1a57aff4486c5c552f256415425ee835c48a4
KillStr3aK <statser07@gmail.com>
2023-11-30 18:46:31 +0100

    feat: PlayerCache<T>

---
+++ Models/PlayerCache.cs
@@ -0,0 +1,32 @@
+﻿namespace CSSharpTests
+{
+    using CounterStrikeSharp.API.Core;
+
+    public class PlayerCache<T> : Dictionary<int, T>
+    {
+        public T this[CCSPlayerController controller]
+        {
+            get
+            {
+                if (base.TryGetValue(controller.UserId!.Value, out T? value))
+                {
+                    return value;
+                }
+
+                return default(T)!;
+            }
+
+            set { this[controller.UserId!.Value] = value; }
+        }
+
+        public bool ContainsPlayer(CCSPlayerController player)
+        {
+            return base.ContainsKey(player.UserId!.Value);
+        }
+
+        public bool RemovePlayer(CCSPlayerController player)
+        {
+            return base.Remove(player.UserId!.Value);
+        }
+    }
+}
