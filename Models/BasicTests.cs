c9ec9712aa4824581f4f691b11a305a89a10b541
KillStr3aK <statser07@gmail.com>
2023-11-30 01:19:38 +0100

    messing with html text

--- Models/BasicTests.cs
+++ Models/BasicTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace CSSharpTests
 {
+    using CounterStrikeSharp.API.Core;
     using CounterStrikeSharp.API.Core.Plugin;
 
     using Microsoft.Extensions.Logging;
@@ -19,6 +20,23 @@
         public void Initialize(bool hotReload)
         {
             this.Logger.LogInformation("Initializing '{0}'", this.GetType().Name);
+
+            Plugin plugin = (this.PluginContext.Plugin as Plugin)!;
+
+            plugin.AddCommand("css_html", "Display html text", (player, info) =>
+            {
+                if (player == null || !player.IsValid)
+                    return;
+
+                var @event = new EventShowSurvivalRespawnStatus(true)
+                {
+                    LocToken = info.GetArg(1),
+                    Duration = 10,
+                    Userid = player
+                };
+
+                @event.FireEventToClient(player);
+            });
         }
 
         public void Release(bool hotReload)
