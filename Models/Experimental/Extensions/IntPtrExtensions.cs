9c4f8dc0f07888c979ba50940f11175ce60ba4ee
KillStr3aK <statser07@gmail.com>
2023-12-01 04:30:29 +0100

    feat: `IntPtr.ToAbsolute`

---
+++ Models/Experimental/Extensions/IntPtrExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CSSharpTests.Experimental
+{
+    public static class IntPtrExtensions
+    {
+        public static IntPtr AddOffset(this IntPtr ptr, int offset)
+        {
+            return ptr + offset;
+        }
+
+        public static unsafe IntPtr ToAbsolute(this IntPtr ptr, int preOffset, int postOffset)
+        {
+            if (ptr != IntPtr.Zero)
+            {
+                ptr.AddOffset(preOffset);
+                ptr = ptr + sizeof(int) + *(int*)(ptr);
+                ptr.AddOffset(postOffset);
+            }
+
+            return ptr;
+        }
+    }
+}
