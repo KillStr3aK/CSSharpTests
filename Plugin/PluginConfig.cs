6aee07858eaaf80c910ab314b69d543ec68c5fae
KillStr3aK <statser07@gmail.com>
2023-12-01 06:25:09 +0100

    feat: `NetworkStateChangedSignature` and `StateChangedSignature`

--- Plugin/PluginConfig.cs
+++ Plugin/PluginConfig.cs
@@ -2,10 +2,12 @@
 {
     using CounterStrikeSharp.API.Core;
 
-    using System.Text.Json.Serialization;
-
     public sealed class PluginConfig : BasePluginConfig
     {
         public WIN_LINUX<string> GetAttributeDefinitionByNameSignature { get; set; } = new WIN_LINUX<string>(string.Empty, string.Empty);
+
+        public WIN_LINUX<string> NetworkStateChangedSignature { get; set; } = new WIN_LINUX<string>(string.Empty, string.Empty);
+
+        public WIN_LINUX<string> StateChangedSignature { get; set; } = new WIN_LINUX<string>(string.Empty, string.Empty);
     }
 }
