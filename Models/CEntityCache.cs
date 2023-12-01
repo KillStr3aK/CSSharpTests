4c0f705ea5d24f317475d40aca8407a9aeab2728
KillStr3aK <statser07@gmail.com>
2023-12-01 06:07:31 +0100

    feat: `CEntityCache<>`

---
+++ Models/CEntityCache.cs
@@ -0,0 +1,31 @@
+﻿namespace CSSharpTests
+{
+    using CounterStrikeSharp.API.Core;
+
+    public class CEntityCache<T> where T : CEntityInstance
+    {
+        private readonly Func<T?> ValueFactory;
+        private Lazy<T?> LazyValue;
+
+        public CEntityCache(Func<T> valueFactory)
+        {
+            ValueFactory = valueFactory;
+            LazyValue = new Lazy<T?>(ValueFactory);
+        }
+
+        public bool IsValid => LazyValue.Value != null && LazyValue.Value.IsValid;
+
+        public T? Value
+        {
+            get
+            {
+                if (!IsValid)
+                {
+                    LazyValue = new Lazy<T?>(() => ValueFactory());
+                }
+
+                return LazyValue.Value;
+            }
+        }
+    }
+}
